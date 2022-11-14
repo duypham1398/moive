748f6dc092cd510fe1f3190bd65157a112553f81
Your Name <you@example.com>
2022-11-14 10:49:28 +0700

    create

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarBrightness: Brightness.dark,
+    ));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          fontFamily: 'montserrat',
+          scaffoldBackgroundColor: DarkTheme.darkerBackground,
+          textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: DarkTheme.white, displayColor: DarkTheme.white)),
+      home: const HomePage(),
+    );
+  }
+}
